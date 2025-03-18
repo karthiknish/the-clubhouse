68b5c0fcdab4736e3d6a4869a76cf23bc64e0992
Karthik <karthik.nishanth06@gmail.com>
2025-03-18 16:54:01 +0530

    fin

--- src/app/page.js
+++ src/app/page.js
@@ -602,22 +602,24 @@ export default function Home() {
             </motion.div>
           </motion.div>
         </section>
-        <iframe
-          id="JotFormIFrame-241052282184046"
-          title="Clubhouse Business App"
-          allowtransparency="true"
-          allow="geolocation; microphone;"
-          src="https://form.jotform.com/241052282184046"
-          frameBorder="0"
-          style={{
-            minWidth: "100%",
-            maxWidth: "100%",
-            height: "539px",
-            border: "none",
-            backgroundColor: "transparent",
-          }}
-          scrolling="no"
-        />
+        <section id="join-form">
+          <iframe
+            id="JotFormIFrame-241052282184046"
+            title="Clubhouse Business App"
+            allowtransparency="true"
+            allow="geolocation; microphone;"
+            src="https://form.jotform.com/241052282184046"
+            frameBorder="0"
+            style={{
+              minWidth: "100%",
+              maxWidth: "100%",
+              height: "539px",
+              border: "none",
+              backgroundColor: "transparent",
+            }}
+            scrolling="no"
+          />
+        </section>
       </div>
     </main>
   );
