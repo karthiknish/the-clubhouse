b066f165b2e56f842ab02dafd6b9a72052b2d3b2
Karthik <karthik.nishanth06@gmail.com>
2025-03-18 16:40:09 +0530

    fin

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,6 @@
 import { motion } from "framer-motion";
 import { FeaturesSection } from "./components/ui/features";
 import { CardSection } from "./components/ui/card";
-
-import ContactForm from "./components/ContactForm";
 // Animation variants
 const fadeIn = {
   hidden: { opacity: 0, y: 20 },
@@ -604,9 +602,22 @@ export default function Home() {
             </motion.div>
           </motion.div>
         </section>
-
-        {/* Contact Form */}
-        <ContactForm id="join-form" staggerChildren={staggerChildren} />
+        <iframe
+          id="JotFormIFrame-241052282184046"
+          title="Clubhouse Business App"
+          allowtransparency="true"
+          allow="geolocation; microphone;"
+          src="https://form.jotform.com/241052282184046"
+          frameBorder="0"
+          style={{
+            minWidth: "100%",
+            maxWidth: "100%",
+            height: "539px",
+            border: "none",
+            backgroundColor: "transparent",
+          }}
+          scrolling="no"
+        />
       </div>
     </main>
   );
